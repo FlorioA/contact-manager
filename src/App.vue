5eea74ffbada84c525da33a77d55eabbf197f55e
FlorioA <adrien.florio@gmail.com>
2020-10-09 13:06:04 +0200

    contact manager

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-4 container">
+          <ContactForm v-on:created="getAllContacts" />
+        </div>
+        <div class="col-8">
+          <ContactList
+            v-bind:contacts="contacts"
+            v-on:delete-contact="deleteContact"
+            v-on:save-edit="saveEdit"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import ContactList from '@/components/ContactList.vue';
+import ContactForm from './components/ContactForm.vue';
+import db from './shared/db';
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      contacts: [],
+    };
+  },
+  created() {
+    this.getAllContacts();
+  },
+  methods: {
+    getAllContacts() {
+      db.read().then((snapshot) => {
+        this.contacts = snapshot.docs;
+      }).catch((error) => {
+        console.error(error);
+      });
+    },
+    deleteContact(contact) {
+      db.delete(contact.id).then(() => {
+        this.getAllContacts();
+      }).catch((error) => {
+        console.error(error);
+      });
+    },
+    saveEdit(editedContact) {
+      db.update(editedContact).then(() => {
+        this.getAllContacts();
+      }).catch((error) => {
+        console.error(error);
+      });
+    },
+  },
+  name: 'app',
   components: {
-    HelloWorld,
+    ContactForm,
+    ContactList,
   },
 };
 </script>
---
+++ src/components/ContactList.vue
@@ -0,0 +1,44 @@
+<template>
+    <!-- <ul>
+      <li v-for="contact in contacts" v-bind:key="contact.id">
+        {{ contact.data().firstName }} {{ contact.data().lastName }}
+        <button v-on:click="deleteContact(contact)" class="btn btn-danger">x</button>
+      </li>
+    </ul> -->
+
+  <div id="accordion" class="col-12">
+    <contact-details
+      v-bind:contact='contact'
+      v-for='contact in contacts'
+      v-bind:key='contact.data().id'
+      v-on:save-edit="saveEdit"
+      v-on:delete-contact="deleteContact"
+    >
+    </contact-details>
+  </div>
+</template>
+
+<script>
+import ContactDetails from '@/components/ContactDetails.vue';
+
+export default {
+  props: ['contacts'],
+  components: {
+    ContactDetails,
+  },
+  methods: {
+    deleteContact(contact) {
+      this.$emit('delete-contact', contact);
+    },
+    saveEdit(editedContact) {
+      this.$emit('save-edit', editedContact);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+li {
+  list-style: none;
+}
+</style>
